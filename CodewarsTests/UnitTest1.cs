4d383cebfa50dc11766fd4aa65a12837494cb3aa
Михаил Ершов <mishevyy@gmail.com>
2022-11-20 14:45:44 +0300

    codewars

---
+++ CodewarsTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace CodewarsTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
